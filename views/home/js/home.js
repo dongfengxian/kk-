abff6f9224e08519e55b9de9c0c73d018473ff4e
dongfengxian <18401569575@163.com>
2017-02-10 13:40:10 +0800

    first

---
+++ views/home/js/home.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 77922 on 2017/2/9.
+ */
+
+
+var mySwiper = new Swiper('.swiper-container',{
+        pagination : '.swiper-pagination',
+//pagination : '#swiper-pagination1',
+    })
